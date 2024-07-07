5d69aa30949cf6ed7da3353fc0ea38a604c7ce03
xtaanum <anumesther15@gmail.com>
2024-07-07 23:45:11 +0000

    change to script file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
-        window.location.href = "index.html"; // Redirect to landing page
+        window.location.href = "landing.html"; // Redirect to landing page
     }, 3000); // Display splash screen for 3 seconds
 });
 
