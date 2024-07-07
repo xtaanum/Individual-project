5c35aea8d664a6c783bf71587ff82009767eb6ca
xtaanum <anumesther15@gmail.com>
2024-07-07 23:40:24 +0000

    changes to index file

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        window.location.href = "index.html"; // Redirect to landing page
+    }, 3000); // Display splash screen for 3 seconds
+});
+
